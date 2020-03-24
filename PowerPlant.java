b9d704af7df6049579e36401aab4fd56520d35f5
k1822405 <43301532+k1822405@users.noreply.github.com>
2020-03-24 18:54:41 -0500

    Update PowerPlant.java

--- PowerPlant.java
+++ PowerPlant.java
@@ -25,14 +25,35 @@ public class PowerPlant {
 		this.minBid = minBid;
 		this.numCitiesPowered = numCitiesPowered;
 	}
-
+	public boolean isHybrid()
+	{
+		if(cost.get(0).contains("."))
+		{
+			return true;
+		}
+		return false;
+	}
 	public void addResources(ArrayList<String> resources) {
 		// if the plant is green
 		if (cost.size() == 0) {
 			return;
 		}
+		if(isHybrid())
+		{
+			String firstResource = cost.get(0).substring(0, cost.get(0).indexOf("."));
+			String secondResource =  cost.get(0).substring(cost.get(0).indexOf("."), cost.get(0).length());
+			int index = 0;
+			while(!hybridFull())
+			{
+				if(resources.get(index).equals(firstResource)||resources.get(index).equals(secondResource)){
+					storage.add(resources.get(index));
+				}
+				++index;
+			}
+		}
 		int index = 0;
-		while (storage.size() <= cost.size()) {
+
+		while (!isFull()) {
 			if (cost.get(0).contains(resources.get(index))) {
 				storage.add(resources.get(index));
 			}
@@ -42,6 +63,38 @@ public class PowerPlant {
 
 	public boolean isFull() {
 		return storage.size() >= (cost.size() * 2);
+	}
+	public boolean hybridFull()
+	{
+		String firstResource = cost.get(0).substring(0, cost.get(0).indexOf("."));
+		int numCostFirstResource = 0;
+		String secondResource =  cost.get(0).substring(cost.get(0).indexOf("."), cost.get(0).length());
+		int numCostSecondResource = 0;
+		//Figuring out how many of each resource the cost asks for
+		for(String resource : cost){
+			if(firstResource.equals(resource)){
+				numCostFirstResource++;
+			}
+			else if(secondResource.equals(resource)){
+				numCostSecondResource++;
+			}
+		}
+		//The number of resources in storage for each specific resource
+		int numStorageFirstResource = 0;
+		int numStorageSecondResource = 0;
+		for(String resource : storage){
+			if(firstResource.equals(resource)){
+				numStorageFirstResource++;
+			}
+			else if(secondResource.equals(resource)){
+				numStorageSecondResource++;
+			}
+		}
+		if(numStorageFirstResource<=numCostFirstResource && numStorageSecondResource<=numCostFirstResource)
+		{
+			return true;
+		}	
+		return false;
 	}
 
 	public boolean burnResources(ArrayList<String> resources) {
