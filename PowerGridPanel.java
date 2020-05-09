51928f5d085b92e051838d8eff6f0c5d33b6894d
DarshShah1008 <darshs1008@gmail.com>
2020-05-09 17:47:49 -0500

    Update PowerGridPanel.java

--- PowerGridPanel.java
+++ PowerGridPanel.java
@@ -304,7 +304,7 @@ public class PowerGridPanel extends JPanel implements MouseListener, KeyListener
 		BufferedImage oil = ImageIO.read(PowerGridPanel.class.getResource("UI/oil.PNG"));
 		int sizeOil=gs.countResource(gs.getOilMarket());
 		for (int i=24-sizeOil+1;i<=24;i++) {
-			g.drawImage(oil, -2+35*i+10*((i-1)/3), 908, 30, 30, null);
+			g.drawImage(oil, 7+25*i+40*((i-1)/3), 945, 30, 30, null);
 		}
 		
 		
